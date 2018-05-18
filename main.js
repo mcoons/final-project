b58629d59afed733916073a53f7d7fd8f2789f3d
Michael Coons <michael@Michaels-MacBook-Pro.local>
2018-05-18 15:26:16 -0600

    cleaned code, validated code

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ function clicked(clickedId){
     var buttons = Array.from( document.getElementsByClassName('navButton') );
     buttons.forEach ( button => button.classList.remove('selectedButton') );
     document.getElementById(clickedId).classList.add('selectedButton');
-
     var activePage = clickedId.toString().replace("Button","");
-
     var pages = Array.from( document.getElementsByClassName('page') );
     pages.forEach( page => page.classList.add('hidden') );
     document.getElementById(activePage).classList.remove('hidden');
